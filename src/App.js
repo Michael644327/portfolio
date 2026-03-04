954d1ea16e08294d263dbdcd9c71c4e7b840c6a8
Michael <165128158+Michael644327@users.noreply.github.com>
2026-03-04 18:25:24 +0800

    設typescript

--- src/App.js
+++ src/App.js
@@ -1,31 +1,11 @@
 import { RouterProvider } from 'react-router-dom';
 import { router } from './routes';
-// import { Routes, Route } from 'react-router-dom';
-
-// import LayoutRoute from './components/LayoutRoute';
 import "./App.css";
-// import routes from './routes/routes';
-// import RouteAP from './routes/routesAP'
 
 function App() {
   return (
-    <>
-    
+    <>  
       <RouterProvider router={router} />
-      {/* <div className='maincontainer'>
-        <main>
-          <Routes>
-            {routes.map(({ path, element: Component, title, h2Title }) => (
-              <Route key={path} path={path} element={
-                <LayoutRoute title={title} showH2={h2Title}>
-                  <Component />
-                </LayoutRoute>} />
-            ))}
-
-            <Route path="/animalpara/*" element={<RouteAP />} />
-          </Routes>
-        </main>
-      </div> */}
     </>
   );
 }
